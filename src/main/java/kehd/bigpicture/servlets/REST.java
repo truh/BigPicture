5aa0f002df6947e266484b1f4df47b2052feef52
Jakob Klepp <jklepp@student.tgm.ac.at>
2014-05-28 19:03:18 +0200

    Commandpattern (Executer)

--- src/main/java/kehd/bigpicture/servlets/REST.java
+++ src/main/java/kehd/bigpicture/servlets/REST.java
@@ -1,9 +1,58 @@
 package kehd.bigpicture.servlets;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by jakob on 5/28/14.
  */
 public class REST extends HttpServlet {
+    /**
+     * Initialize PrimeSearcher Servlet
+     */
+    @Override
+    public void init() {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws
+            ServletException,
+            IOException
+    {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws
+            ServletException,
+            IOException
+    {
+        processRequest(request, response);
+    }
+
+    /**
+     * Answers both get and post requests
+     *
+     * @param request request
+     * @param response  response callback
+     * @throws IOException
+     */
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws
+            IOException
+    {
+        PrintWriter out = response.getWriter();
+        out.println();
+    }
+
+    @Override
+    public void destroy()
+    {
+    }
 }
