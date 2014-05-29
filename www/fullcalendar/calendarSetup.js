9cb416a8fffadb18001e101a98e8b91b78432f7d
mhaidn-tgm <mhaidn@student.tgm.ac.at>
2014-05-29 11:55:49 +0200

    DateTimePicker / Datamodel

--- www/fullcalendar/calendarSetup.js
+++ www/fullcalendar/calendarSetup.js
@@ -1,12 +1,20 @@
 /* FullCalendar-Setup for BigPicture
+ * This file contains all functions to create, delete and
+ * update events from the calendar.
+ *
  * Martin Haidn
  * 2014-05-12
 */
 
 /* This Method is setting up a new calender with our specified functionalities.
  * It also provides all the callback-interfaces which are used to interact with the calander like
- * adding new events, getting informed on click-events and more
+ * adding new events, getting informed on click-events and more.
 */
+
+//global vars to hold the information from the callbacks
+var currentStart;
+var currentEnd;
+
 $(document).ready(function() {
 	
 	var date = new Date();
@@ -28,22 +36,23 @@ $(document).ready(function() {
 		selectHelper: true,
 
 		//functions and callback interfaces
+
+		//Callback if an event was clicked
+		eventClick: function(calEvent, jsEvent, view) {
+
+			alert('Youre Event: ' + calEvent.title);
+
+   		},
+
+		//Callback if a day was clicked
 		select: function(start, end, allDay) {
-			var title = prompt('Event Title:');
-			if (title) {
-				calendar.fullCalendar('renderEvent',
-					{
-						title: title,
-						start: start,
-						end: end,
-						allDay: allDay
-					},
-					true // make the event "stick"
-				);
-			}
-			calendar.fullCalendar('unselect');
+			lightbox_open();
+			currentStart= start;
+			currentEnd= end;
 		},
-		editable: true,
+
+
+
 		events: [
 			{
 				title: 'test',
@@ -53,3 +62,42 @@ $(document).ready(function() {
 	});
 		
 });
+
+function getFormValues() {
+	$('#title')
+}
+
+/* Adding a new event to the calendar
+ *
+ * Martin Haidn
+ * 28.05.2014
+*/
+
+function addEvent() {
+
+	//Reading eventdetails from user form
+	var title= $('#title').val();
+	var loc= $('#location').val();
+	var desc= $('#description').val();
+
+	if (title) {
+		$('#calendar').fullCalendar('renderEvent', {
+			title: title,
+			start: currentStart,
+			end: currentEnd,
+			allDay: true
+		});
+		true // make the event "stick"
+	} else {
+		alert("Please choose a title!");
+	}
+	lightbox_close();
+
+}
+
+function eventTest() {
+	$('#calendar').fullCalendar('renderEvent', {
+		title: 'MeinEvent',
+		start: "2014-05-07T13:30:00.000Z"
+	});
+}
