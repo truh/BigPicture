6b77d51b8ec029992af4a9c4813fe80fa2ae2698
Jakob Klepp <jklepp@student.tgm.ac.at>
2014-06-03 17:26:58 +0200

    test code aus REST entfernt

--- src/main/java/kehd/bigpicture/servlets/REST.java
+++ src/main/java/kehd/bigpicture/servlets/REST.java
@@ -24,18 +24,12 @@ import java.util.Map;
 public class REST extends HttpServlet {
     private static final Logger log = Logger.getLogger(REST.class);
 
-    private boolean test;
     private Executor executor;
 
     @Override
     public void init() {
         log.info("Initialising REST Servlet");
 
-        // check if servlet should be started in testmode
-        test = false;
-        if(System.getProperty("bp-test") != null) {
-            test = System.getProperty("bp-test").toLowerCase().equals("true");
-        }
 
         if(!test) {
             executor = new Executor(); 
@@ -72,79 +66,12 @@ public class REST extends HttpServlet {
             throws
             IOException
     {
-        log.info("AuthType: " + request.getAuthType());
-        log.info("REST#processRequest");
-
         String methodName = request.getParameter("method");
         methodName = methodName==null?"":methodName;
 
         response.setContentType("application/json");
 
         PrintWriter out = response.getWriter();
-        if(test) {
-            switch (methodName) {
-                case "getEvents":
-                    out.write("{\"error\":\"None\", \"result\":[" +
-                            "{" +
-                            "   \"title\"       :\"BigPicture Coden\"," +
-                            "   \"owner\"       :\"Martin\"," +
-                            "   \"appointments\":[" +
-                            "       \"2014-05-29T10:00:00\"," +
-                            "       \"2014-05-31T10:00:00\"," +
-                            "       \"2014-06-01T10:00:00\"" +
-                            "   ]," +
-                            "   \"participants\":[" +
-                            "       {\"username\":\"Martin\"," +
-                            "        \"date\":\"2014-05-29T10:00:00\"}," +
-                            "       {\"username\":\"Matthias\"," +
-                            "        \"date\":\"2014-05-29T10:00:00\"}," +
-                            "       {\"username\":\"Daniel\"," +
-                            "        \"date\":\"2014-05-29T10:00:00\"}," +
-                            "       {\"username\":\"Jakob\"," +
-                            "        \"date\":\"2014-05-29T10:00:00\"}" +
-                            "   ]," +
-                            "   \"comments\"    :[" +
-                            "       {\"timestamp\":\"2014-05-28T10:00:00\"," +
-                            "        \"author\":\"Matthias\"," +
-                            "        \"content\":\"Blulluuub\"}" +
-                            "   ]" +
-                            "}," +
-                            "{" +
-                            "   \"title\"       :\"[ITP] Test\"," +
-                            "   \"owner\"       :\"wkristufek\"," +
-                            "   \"appointments\":[" +
-                            "       \"2014-06-03T10:40:00\"" +
-                            "   ]," +
-                            "   \"participants\":[" +
-                            "   ]," +
-                            "   \"comments\"    :[" +
-                            "   ]" +
-                            "}," +
-                            "{" +
-                            "   \"title\"       :\"[D] Test Epochen\"," +
-                            "   \"owner\"       :\"Martin\"," +
-                            "   \"appointments\":[" +
-                            "       \"2014-06-03T13:00:00\"" +
-                            "   ]," +
-                            "   \"participants\":[]," +
-                            "   \"comments\"    :[]" +
-                            "}," +
-                            "{" +
-                            "   \"title\"       :\"[GGP] Geschichte Test\"," +
-                            "   \"owner\"       :\"Martin\"," +
-                            "   \"appointments\":[" +
-                            "       \"2014-06-04T12:00:00\"" +
-                            "   ]," +
-                            "   \"participants\":[]," +
-                            "   \"comments\"    :[]" +
-                            "}" +
-                            "]}");
-                    break;
-                default:
-                    out.println("{\"error\":{" +
-                            "\"message\":\"Method not found\"" +
-                            "\"code\":0" +
-                            "}, \"result\":\"None\"}");
             }
         } else {
             if(executor.getCommandNames().contains(methodName)) {
