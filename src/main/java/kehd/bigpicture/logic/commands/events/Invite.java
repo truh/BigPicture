16ea541855a2ab0b9981e234500c14dba19bcdae
ddjuric-tgm <ddjuric@student.tgm.ac.at>
2014-06-04 19:48:34 +0200

    JUNIT tests

--- src/main/java/kehd/bigpicture/logic/commands/events/Invite.java
+++ src/main/java/kehd/bigpicture/logic/commands/events/Invite.java
@@ -13,60 +13,61 @@ import java.util.Map;
 import static argo.jdom.JsonNodeBuilders.aStringBuilder;
 
 public class Invite implements Command {
-    private EntityManagerFactory entityManagerFactory;
+	private EntityManagerFactory entityManagerFactory;
 
-    public Invite(EntityManagerFactory entityManagerFactory) {
-        this.entityManagerFactory = entityManagerFactory;
-    }
+	public Invite(EntityManagerFactory entityManagerFactory) {
+		this.entityManagerFactory = entityManagerFactory;
+	}
 
-    @Override
-    public JsonNodeBuilder execute(String username, Map<String, String> params)
-            throws NoSuchElement, NotAuthentificated, NotAuthorized, DateInvalid {
-        // check if logged in
-        if(username == null) {
-            throw new NotAuthentificated();
-        }
+	@Override
+	public JsonNodeBuilder execute(String username, Map<String, String> params)
+			throws NoSuchElement, NotAuthentificated, NotAuthorized,
+			DateInvalid {
+		// check if logged in
+		if (username == null) {
+			throw new NotAuthentificated();
+		}
 
-        // ueberpruefen ob parameter gesetzt sind
-        String eventName = params.get("eventName");
-        if(eventName == null) {
-            throw new NoSuchElement("elementName");
-        }
+		// ueberpruefen ob parameter gesetzt sind
+		String eventName = params.get("eventName");
+		if (eventName == null) {
+			throw new NoSuchElement("elementName");
+		}
 
-        String userName = params.get("user");
-        if(userName == null) {
-            throw new NoSuchElement("user");
-        }
+		String userName = params.get("user");
+		if (userName == null) {
+			throw new NoSuchElement("user");
+		}
 
-        EntityManager manager = entityManagerFactory.createEntityManager();
+		EntityManager manager = entityManagerFactory.createEntityManager();
 
-        manager.getTransaction().begin();
+		manager.getTransaction().begin();
 
-        Event event = manager.createQuery(
-                "SELECT DISTINCT Event FROM Event " +
-                        "WHERE Event.title = :eventName", Event.class)
-                .setParameter("eventName", eventName)
-                .getSingleResult();
+		Event event = manager
+				.createQuery(
+						"SELECT DISTINCT Event FROM Event "
+								+ "WHERE Event.title = :eventName", Event.class)
+				.setParameter("eventName", eventName).getSingleResult();
 
-        if(!event.getOrganisator().getName().equals(username)) {
-            throw new NotAuthorized("Invite");
-        }
+		if (!event.getOrganisator().getName().equals(username)) {
+			throw new NotAuthorized("Invite");
+		}
 
-        User user = manager.createQuery(
-                "SELECT DISTINCT User FROM User " +
-                        "WHERE User.name = :userName", User.class)
-                .setParameter("userName", userName)
-                .getSingleResult();
+		User user = manager
+				.createQuery(
+						"SELECT DISTINCT User FROM User "
+								+ "WHERE User.name = :userName", User.class)
+				.setParameter("userName", userName).getSingleResult();
 
-        if(user == null) {
-            throw new UserDoesNotExist();
-        }
+		if (user == null) {
+			throw new UserDoesNotExist();
+		}
 
-        event.getUsers().add(user);
-        manager.persist(event);
+		event.getUsers().add(user);
+		manager.persist(event);
 
-        manager.getTransaction().commit();
+		manager.getTransaction().commit();
 
-        return aStringBuilder("Okay!");
-    }
+		return aStringBuilder("Okay!");
+	}
 }
--- src/test/java/kehd/bigpicture/test/logic/commands/notifications/GetNotifications_Test.java
+++ src/test/java/kehd/bigpicture/test/logic/commands/notifications/GetNotifications_Test.java
@@ -22,89 +22,99 @@ import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
 public class GetNotifications_Test {
-    @Test
-    public void execution() {
-        EntityManagerFactory emf = mock(EntityManagerFactory.class);
-        doReturn(new EntityManagerAdapter() {
-            @Override
-            public <T> TypedQuery<T> createQuery(String qlString, Class<T> resultClass) {
-                TypedQuery<T> query = mock(TypedQuery.class);
+	@Test
+	public void execution() {
+		EntityManagerFactory emf = mock(EntityManagerFactory.class);
+		doReturn(new EntityManagerAdapter() {
+			@Override
+			public <T> TypedQuery<T> createQuery(String qlString,
+					Class<T> resultClass) {
+				TypedQuery<T> query = mock(TypedQuery.class);
 
-                ArrayList<Notification> notifications = new ArrayList<Notification>(){{
-                    Event e1 = new Event();
-                    e1.setId(1);
-                    e1.setTitle("Event1");
+				ArrayList<Notification> notifications = new ArrayList<Notification>() {
+					{
+						Event e1 = new Event();
+						e1.setId(1);
+						e1.setTitle("Event1");
 
-                    Event e2 = new Event();
-                    e2.setId(2);
-                    e2.setTitle("Event2");
+						Event e2 = new Event();
+						e2.setId(2);
+						e2.setTitle("Event2");
 
-                    ArrayList<User> recipients = new ArrayList<User>() {{
-                        User johnDoe = new User();
-                        johnDoe.setName("JohnDoe");
-                        add(johnDoe);
-                    }};
+						ArrayList<User> recipients = new ArrayList<User>() {
+							{
+								User johnDoe = new User();
+								johnDoe.setName("JohnDoe");
+								add(johnDoe);
+							}
+						};
 
-                    Notification n1 = new Notification();
-                    n1.setId(1);
-                    n1.setMessage("Message1");
-                    n1.setEvent(e1);
-                    n1.setTimestamp(new Date());
-                    n1.setRecipients(recipients);
-                    n1.setType(NotificationType.NEW_INVITATION);
-                    add(n1);
+						Notification n1 = new Notification();
+						n1.setId(1);
+						n1.setMessage("Message1");
+						n1.setEvent(e1);
+						n1.setTimestamp(new Date());
+						n1.setRecipients(recipients);
+						n1.setType(NotificationType.NEW_INVITATION);
+						add(n1);
 
-                    Notification n2 = new Notification();
-                    n2.setId(2);
-                    n2.setMessage("Message2");
-                    n2.setEvent(e1);
-                    n2.setTimestamp(new Date());
-                    n2.setRecipients(recipients);
-                    n2.setType(NotificationType.NEW_INVITATION);
-                    add(n2);
+						Notification n2 = new Notification();
+						n2.setId(2);
+						n2.setMessage("Message2");
+						n2.setEvent(e1);
+						n2.setTimestamp(new Date());
+						n2.setRecipients(recipients);
+						n2.setType(NotificationType.NEW_INVITATION);
+						add(n2);
 
-                    Notification n3 = new Notification();
-                    n3.setId(3);
-                    n3.setMessage("Message3");
-                    n3.setEvent(e2);
-                    n3.setTimestamp(new Date());
-                    n3.setRecipients(recipients);
-                    n3.setType(NotificationType.NEW_INVITATION);
-                    add(n3);
+						Notification n3 = new Notification();
+						n3.setId(3);
+						n3.setMessage("Message3");
+						n3.setEvent(e2);
+						n3.setTimestamp(new Date());
+						n3.setRecipients(recipients);
+						n3.setType(NotificationType.NEW_INVITATION);
+						add(n3);
 
-                    Notification n4 = new Notification();
-                    n4.setId(4);
-                    n4.setMessage("Message4");
-                    n4.setEvent(e2);
-                    n4.setTimestamp(new Date());
-                    n4.setRecipients(new ArrayList<User>());
-                    n4.setType(NotificationType.NEW_INVITATION);
-                    add(n4);
-                }};
+						Notification n4 = new Notification();
+						n4.setId(4);
+						n4.setMessage("Message4");
+						n4.setEvent(e2);
+						n4.setTimestamp(new Date());
+						n4.setRecipients(new ArrayList<User>());
+						n4.setType(NotificationType.NEW_INVITATION);
+						add(n4);
+					}
+				};
 
-                doReturn(notifications).when(query).getResultList();
+				doReturn(notifications).when(query).getResultList();
 
-                return query;
-            }
-        })
-                .when(emf)
-                .createEntityManager();
-        GetNotifications getNotifications = new GetNotifications(emf);
+				return query;
+			}
+		}).when(emf).createEntityManager();
+		GetNotifications getNotifications = new GetNotifications(emf);
 
-        JsonNodeBuilder nodeBuilder = getNotifications.execute("JohnDoe", new HashMap<String, String>());
+		JsonNodeBuilder nodeBuilder = getNotifications.execute("JohnDoe",
+				new HashMap<String, String>());
 
-        assertNotNull("Sollte nicht null zurückgeben.", nodeBuilder);
+		assertNotNull("Sollte nicht null zurückgeben.", nodeBuilder);
 
-        List<JsonNode> nodes = nodeBuilder.build().getElements();
+		List<JsonNode> nodes = nodeBuilder.build().getElements();
 
-        assertSame("Laenge sollte wie gegeben sein.", 3, nodes.size());
+		assertSame("Laenge sollte wie gegeben sein.", 3, nodes.size());
 
-        assertEquals("Nachricht sollte wie gegeben sein.", "Message1", nodes.get(0).getStringValue("message"));
-        assertEquals("Nachricht sollte wie gegeben sein.", "Message2", nodes.get(1).getStringValue("message"));
-        assertEquals("Nachricht sollte wie gegeben sein.", "Message3", nodes.get(2).getStringValue("message"));
+		assertEquals("Nachricht sollte wie gegeben sein.", "Message1", nodes
+				.get(0).getStringValue("message"));
+		assertEquals("Nachricht sollte wie gegeben sein.", "Message2", nodes
+				.get(1).getStringValue("message"));
+		assertEquals("Nachricht sollte wie gegeben sein.", "Message3", nodes
+				.get(2).getStringValue("message"));
 
-        assertEquals("Id sollte wie gegeben sein.", "1", nodes.get(0).getNumberValue("id"));
-        assertEquals("Id sollte wie gegeben sein.", "2", nodes.get(1).getNumberValue("id"));
-        assertEquals("Id sollte wie gegeben sein.", "3", nodes.get(2).getNumberValue("id"));
-    }
+		assertEquals("Id sollte wie gegeben sein.", "1", nodes.get(0)
+				.getNumberValue("id"));
+		assertEquals("Id sollte wie gegeben sein.", "2", nodes.get(1)
+				.getNumberValue("id"));
+		assertEquals("Id sollte wie gegeben sein.", "3", nodes.get(2)
+				.getNumberValue("id"));
+	}
 }
