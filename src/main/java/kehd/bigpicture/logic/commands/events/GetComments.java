557588d1d3d15ce5fc534a45935ae06087dc982e
Jakob Klepp <jklepp@student.tgm.ac.at>
2014-06-04 16:06:10 +0200

    GetComments

--- src/main/java/kehd/bigpicture/logic/commands/events/GetComments.java
+++ src/main/java/kehd/bigpicture/logic/commands/events/GetComments.java
@@ -1,10 +1,20 @@
 package kehd.bigpicture.logic.commands.events;
 
+import argo.jdom.JsonArrayNodeBuilder;
 import argo.jdom.JsonNodeBuilder;
+import kehd.bigpicture.exceptions.NoSuchElement;
 import kehd.bigpicture.logic.commands.Command;
+import kehd.bigpicture.model.Comment;
+import kehd.bigpicture.model.Event;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import java.util.List;
 import java.util.Map;
+
+import static argo.jdom.JsonNodeBuilders.aStringBuilder;
+import static argo.jdom.JsonNodeBuilders.anArrayBuilder;
+import static argo.jdom.JsonNodeBuilders.anObjectBuilder;
 
 public class GetComments implements Command {
     private EntityManagerFactory entityManagerFactory;
@@ -14,7 +24,38 @@ public class GetComments implements Command {
     }
 
     @Override
-    public JsonNodeBuilder execute(String username, Map<String, String> params) {
-        return null;
+    public JsonNodeBuilder execute(String username, Map<String, String> params)
+            throws NoSuchElement {
+        String eventName = params.get("eventName");
+
+        EntityManager manager = entityManagerFactory.createEntityManager();
+
+        // check if event exists
+        Event event = manager.createQuery(
+                "SELECT DISTINCT Event " +
+                        "FROM Event " +
+                        "WHERE Event.title = :eventName", Event.class).getSingleResult();
+
+        if(event == null) {
+            throw new NoSuchElement("Event");
+        }
+
+        List<Comment> comments = manager.createQuery(
+                "SELECT Comment " +
+                        "FROM Comment " +
+                        "WHERE Comment.event = :event ", Comment.class)
+                .setParameter("event", event).getResultList();
+
+        JsonArrayNodeBuilder arrayNodeBuilder = anArrayBuilder();
+        for(Comment comment: comments) {
+            arrayNodeBuilder.withElement(
+                    anObjectBuilder()
+                            .withField("timestamp", aStringBuilder(DATE_FORMAT.format(comment.getTimestamp())))
+                            .withField("comment", aStringBuilder(comment.getComment()))
+                            .withField("author", aStringBuilder(comment.getAuthor().getName()))
+            );
+        }
+
+        return arrayNodeBuilder;
     }
 }
