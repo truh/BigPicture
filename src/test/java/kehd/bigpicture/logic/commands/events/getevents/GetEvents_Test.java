772a24231652e1411776f2d1e86ca0e564c84a88
ddjuric-tgm <ddjuric@student.tgm.ac.at>
2014-06-04 18:28:05 +0200

    junit

--- src/test/java/kehd/bigpicture/logic/commands/events/getevents/GetEvents_Test.java
+++ src/test/java/kehd/bigpicture/logic/commands/events/getevents/GetEvents_Test.java
@@ -1,21 +1,22 @@
 package kehd.bigpicture.logic.commands.events.getevents;
+import java.util.Map;
+
+import javax.persistence.EntityManagerFactory;
+
 import kehd.bigpicture.logic.commands.events.*;
 
 import static org.junit.Assert.*;
 
+import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class GetEvents_Test {
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
+	 private Logger log = Logger.getLogger(ReplyInvitation.class);
+	    public Integer executionCount = 0;
+	    public Map paramMap;
+	    private EntityManagerFactory entityManagerFactory;
 
 	@Test
 	public void test() {
